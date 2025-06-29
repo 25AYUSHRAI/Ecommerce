fb8f39f6b9608a928fc47ca7a7ff74983e912751
25AYUSHRAI <ayushrai252005@gmail.com>
2025-06-29 14:34:31 +0530

    File Structure and Backend server complete

---
+++ Ecommerce-Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import axios from "../src/api/axiosconfig"
+const App = () => {
+   const getproducts = async ()=>{
+    try{
+      const res= await axios.get("/products");
+      console.log(res)
+    }
+    catch(error){
+        console.log(error);
+    }
+   }
+   useEffect(()=>{getproducts()},[])
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
